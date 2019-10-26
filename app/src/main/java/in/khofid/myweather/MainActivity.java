8ae23e4606928e0bc9aca9cadac3a038529e4ec1
Khofidin <offiedz@gmail.com>
2019-10-27 06:40:11 +0700

    Add temperature function

--- app/src/main/java/in/khofid/myweather/MainActivity.java
+++ app/src/main/java/in/khofid/myweather/MainActivity.java
@@ -1,14 +1,74 @@
 package in.khofid.myweather;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WeatherAdapter adapter;
+    private EditText edtCity;
+    private ProgressBar progressBar;
+    private MainViewModel mainViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        mainViewModel.getWeathers().observe(this, getWeather);
+
+        adapter = new WeatherAdapter();
+        adapter.notifyDataSetChanged();
+
+        RecyclerView  recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        edtCity = findViewById(R.id.editCity);
+        progressBar = findViewById(R.id.progressBar);
+        findViewById(R.id.btnCity).setOnClickListener(btnCityListener);
+    }
+
+    private Observer<ArrayList<WeatherItems>> getWeather = new Observer<ArrayList<WeatherItems>>() {
+        @Override
+        public void onChanged(ArrayList<WeatherItems> items) {
+            if(getWeather != null) {
+                adapter.setData(items);
+                showLoading(false);
+            }
+        }
+    };
+
+    View.OnClickListener btnCityListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            String city = edtCity.getText().toString();
+
+            if(TextUtils.isEmpty(city)) return;
+
+            mainViewModel.setWeather(city);
+            showLoading(true);
+        }
+    };
+
+    private void showLoading(boolean state) {
+        if (state) {
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+        }
     }
 }
